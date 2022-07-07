f604484e9fc0e9da90df75d430b86588a8b2eecc
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2022-07-07 19:31:33 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -79,7 +79,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://gg.gg/hfrcs");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
