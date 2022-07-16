eb60034e0909ef3ef3cf4cdaf0efc6e5cd75ea98
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2022-07-17 03:01:54 +0800

    Add files via upload

--- app/src/main/java/com/github/tvbox/osc/player/thirdparty/browser.java
+++ app/src/main/java/com/github/tvbox/osc/player/thirdparty/browser.java
@@ -16,43 +16,43 @@ import org.json.JSONObject;
 
 import java.util.HashMap;
 
-public class ucplayer {
-    public static final String TAG = "ThirdParty.uc";
+public class browser {
+    public static final String TAG = "ThirdParty.browser";
 
     private static final String PACKAGE_NAME = "com.UCMobile";
     private static final String PLAYBACK_ACTIVITY = "com.UCMobile.main.UCMobile";
 
-    private static class ucPackageInfo {
+    private static class browserPackageInfo {
         final String packageName;
         final String activityName;
 
-        ucPackageInfo(String packageName, String activityName) {
+        browserPackageInfo(String packageName, String activityName) {
             this.packageName = packageName;
             this.activityName = activityName;
         }
     }
 
-    private static final ucPackageInfo[] PACKAGES = {
-            new ucPackageInfo(PACKAGE_NAME, PLAYBACK_ACTIVITY),
+    private static final browserPackageInfo[] PACKAGES = {
+            new browserPackageInfo(PACKAGE_NAME, PLAYBACK_ACTIVITY),
     };
 
-    public static ucPackageInfo getPackageInfo() {
-        for (ucPackageInfo pkg : PACKAGES) {
+    public static browserPackageInfo getPackageInfo() {
+        for (browserPackageInfo pkg : PACKAGES) {
             try {
                 ApplicationInfo info = App.getInstance().getPackageManager().getApplicationInfo(pkg.packageName, 0);
                 if (info.enabled)
                     return pkg;
                 else
-                    Log.v(TAG, "ucplayer package `" + pkg.packageName + "` is disabled.");
+                    Log.v(TAG, "browser package `" + pkg.packageName + "` is disabled.");
             } catch (PackageManager.NameNotFoundException ex) {
-                Log.v(TAG, "ucplayer package `" + pkg.packageName + "` does not exist.");
+                Log.v(TAG, "browser package `" + pkg.packageName + "` does not exist.");
             }
         }
         return null;
     }
 
     public static boolean run(Activity activity, String url, String title, String subtitle, HashMap<String, String> headers) {
-        ucPackageInfo packageInfo = getPackageInfo();
+        browserPackageInfo packageInfo = getPackageInfo();
         if (packageInfo == null)
             return false;
 
@@ -62,26 +62,26 @@ public class ucplayer {
         intent.setData(Uri.parse(url));
         intent.putExtra("title", title);
         intent.putExtra("name", title);
-        intent.putExtra("uc.extra.title", title);
+        intent.putExtra("browser.extra.title", title);
         if (headers != null && headers.size() > 0) {
             try {
                 JSONObject json = new JSONObject();
                 for (String key : headers.keySet()) {
                     json.put(key, headers.get(key).trim());
                 }
-                intent.putExtra("uc.extra.http_header", json.toString());
+                intent.putExtra("browser.extra.http_header", json.toString());
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
         if (subtitle != null && !subtitle.isEmpty()) {
-            intent.putExtra("uc.extra.subtitle", subtitle);
+            intent.putExtra("browser.extra.subtitle", subtitle);
         }
         try {
-            activity.startActivity(intent);
+            activity.startActivity(Intent.createChooser(intent,getString(R.string.about_cherry_choice_browser)));
             return true;
         } catch (ActivityNotFoundException ex) {
-            Log.e(TAG, "Can't run ucplayer", ex);
+            Log.e(TAG, "Can't run browser", ex);
             return false;
         }
     }
